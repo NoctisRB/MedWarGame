f9f218266621412334d59b07736a5799e804ed59
erkeTeF <erkeTeFo@users.noreply.github.com>
2019-05-04 00:08:55 +0200

    Radius Spawned and Despawned according to isSelected variable

--- MedWarGame/Assets/Code/treeScript.cs
+++ MedWarGame/Assets/Code/treeScript.cs
@@ -5,10 +5,34 @@ using UnityEngine;
 public class treeScript : MonoBehaviour
 {
     // Start is called before the first frame update
-    [SerializeField] private float energyPerSecond = 1f;   
+    [SerializeField] private float energyPerSecond = 1f;
 
     public bool isPlayer = true;
     public bool isSelected;
+    public GameObject radius;
+    private Vector3 radiusTargetScale;
+
+    private void Start()
+    {
+        radiusTargetScale = radius.transform.localScale;
+        radius.transform.localScale = Vector3.zero;
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (isSelected)
+                isSelected = false;
+            else
+                isSelected = true;
+        }
+
+        if (isSelected)
+            radius.transform.localScale = Vector3.Lerp(radius.transform.localScale, radiusTargetScale, 0.1f);
+        else
+            radius.transform.localScale = Vector3.Lerp(radius.transform.localScale, Vector3.zero, 0.1f);
+    }
+
 
     public float GetEnergy()
     {
