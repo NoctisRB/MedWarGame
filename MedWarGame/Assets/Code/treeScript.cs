6c89ce8d9a5316ff87187e679b88f7996a9baf2f
erkeTeF <erkeTeFo@users.noreply.github.com>
2019-05-03 21:31:30 +0200

    TreeScript

---
+++ MedWarGame/Assets/Code/treeScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class treeScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private float energyPerSecond = 1f;
+    //private float currentEnergy;
+    public bool isPlayer = true;
+    public bool isSelected;   
+    public float GetEnergy()
+    {
+        return energyPerSecond;
+    }
+}
