1bc2cda239e262745da4c1c9d3aa3d3888d0063a
Galmoli <tonigalmes98@gmail.com>
2019-05-04 16:56:43 +0200

    Scripts per controlar la UI

--- MedWarGame/Assets/Code/Scripts/CurrencyManager.cs
+++ MedWarGame/Assets/Code/Scripts/CurrencyManager.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class CurrencyManager : MonoBehaviour
 {
+    [HideInInspector] public float currentCurrency = 0;
+    [SerializeField] private GameObject treesParent;
+    private treeScript[] trees = null;
     // Start is called before the first frame update
     void Start()
     {
-        
+        trees = treesParent.GetComponentsInChildren<treeScript>();
+        currentCurrency = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        currentCurrency += GetCurrentCurrencyPerSec() * Time.deltaTime;
+    }
+
+    public float GetCurrentCurrencyPerSec()
+    {
+        float energy = 0;
+        foreach(treeScript t in trees)
+        {
+            if (t.isPlayer) energy += t.GetEnergy();
+        }
+        return energy;
     }
 }
