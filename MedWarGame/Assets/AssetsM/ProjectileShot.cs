f05148c390cab4d4e7467af9e96023e4dd4bff34
Martigt <martivdg@gmail.com>
2019-05-04 18:34:48 +0200

    Projectile shot
    Necessita el game object d'on prové per saber la posició inicial i té un cooldown de 2 segons fins a que es destrueix

---
+++ MedWarGame/Assets/AssetsM/ProjectileShot.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileShot : MonoBehaviour
+{
+    public GameObject bulletEmitter;
+    [HideInInspector] public Vector3 forward = new Vector3();
+
+    private float cooldown = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.transform.position = bulletEmitter.transform.position;
+    }
+    
+
+    void FixedUpdate()
+    {
+        if (cooldown < 0)
+        {
+            this.transform.position += forward.normalized * Time.fixedDeltaTime;
+            cooldown -= Time.fixedDeltaTime;
+        }
+        else Destroy(this);
+    }
+    
+}
