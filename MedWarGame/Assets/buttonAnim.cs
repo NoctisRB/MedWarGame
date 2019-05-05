23b1ff9a508011a49b119718d25fb351b1092a5f
Galmoli <tonigalmes98@gmail.com>
2019-05-05 02:04:55 +0200

    Lerp cartas

---
+++ MedWarGame/Assets/buttonAnim.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*public class buttonAnim : MonoBehaviour, ISelectHandler, IPointerEnterHandler
+{
+    // When highlighted with mouse.
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        // Do something.
+        Debug.Log("<color=red>Event:</color> Completed mouse highlight.");
+    }
+    // When selected.
+    public void OnSelect(BaseEventData eventData)
+    {
+        // Do something.
+        Debug.Log("<color=red>Event:</color> Completed selection.");
+    }
+}
+*/
---
+++ MedWarGame/Assets/buttonAnimation.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonAnimation : MonoBehaviour
+{
+    [HideInInspector] public bool isMoving;
+    [HideInInspector] public bool up;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isMoving = false;
+        up = false;
+    }
+}
