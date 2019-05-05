d3f0bda9b2a9fa9aad3f9ac23680b7c4b9f09a21
JuliaBlasco <47471443+JuliaBlasco@users.noreply.github.com>
2019-05-05 11:53:39 +0200

    yb45

--- MedWarGame/Assets/Code/Scripts/GameLogic.cs
+++ MedWarGame/Assets/Code/Scripts/GameLogic.cs
@@ -25,7 +25,19 @@ public class GameLogic : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKey(KeyCode.Space))
+        _enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        _enemyBases = GameObject.FindGameObjectsWithTag("EnemyBase");
+
+        if (_enemies == null && _enemyBases == null)
+        {
+            string minutes = ((int)timer / 60).ToString();
+            string seconds = Mathf.Round((timer % 60)).ToString();
+
+            timerText.text = "Your time was\n " + minutes + "' " + seconds + "''";
+            _endPanel.SetActive(true);
+        }
+
+        else if (Input.GetKey(KeyCode.Space))
         {
             if (_enemies == null && _enemyBases == null)
             {
@@ -36,21 +48,7 @@ public class GameLogic : MonoBehaviour
                 _endPanel.SetActive(true);
             }          
         }
-        else if (_enemies == null && _enemyBases == null)
-        {
-            string minutes = ((int)timer / 60).ToString();
-            string seconds = Mathf.Round((timer % 60)).ToString();
 
-            timerText.text = "Your time was\n " + minutes + "' " + seconds + "''";
-            _endPanel.SetActive(true);
-        }
-        else
-        {
-            timer += Time.deltaTime;
-        }
-        
-
-        _enemies = GameObject.FindGameObjectsWithTag("Enemy");
-        _enemyBases = GameObject.FindGameObjectsWithTag("EnemyBase");   
+        else { timer += Time.deltaTime; }       
     }
 }
