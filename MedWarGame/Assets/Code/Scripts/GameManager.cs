7913df78a688611d866c4749ecc5854f4e0f479c
JuliaBlasco <47471443+JuliaBlasco@users.noreply.github.com>
2019-05-05 12:22:28 +0200

    gugk

--- MedWarGame/Assets/Code/Scripts/GameManager.cs
+++ MedWarGame/Assets/Code/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -9,16 +10,27 @@ public class GameManager : MonoBehaviour
     [SerializeField] private GameObject winPanel;
     [SerializeField] private GameObject defeatPanel;
     private bool allDestroyed;
+
+
+    [SerializeField]
+    private Text timerText;
+    private float timer = 0;
+
     // Update is called once per frame
     void Start()
     {
         trees = treesParent.GetComponentsInChildren<treeScript>();
+       
+        Time.timeScale = 1;
     }
 
     private void Update()
     {
+
+        timer += Time.deltaTime;
+
         allDestroyed = false;
-        foreach(treeScript t in trees)
+        foreach (treeScript t in trees)
         {
             if (t.isPlayer && t.GetHp() <= 0) Lose();
             if (!t.isPlayer && t.GetHp() > 0)
@@ -26,16 +38,25 @@ public class GameManager : MonoBehaviour
                 allDestroyed = false;
                 return;
             }
-            if (!t.isPlayer && t.GetHp() <= 0) allDestroyed = true;
+            if (!t.isPlayer && t.GetHp() <= 0)
+            {
+                allDestroyed = true;
+            }
         }
-        if (allDestroyed) Win();
+        if (allDestroyed) Win();   
     }
 
     private void Win()
     {
         Time.timeScale = 0;
+
+        string minutes = ((int)timer / 60).ToString();
+        string seconds = Mathf.Round((timer % 60)).ToString();
+
+        timerText.text = "Your time was\n " + minutes + "' " + seconds + "''";
         winPanel.SetActive(true);
     }
+
     private void Lose()
     {
         Time.timeScale = 0;
