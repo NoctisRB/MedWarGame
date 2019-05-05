cdb47f48023cd8c0c3f6efff5e08174c38d2e178
erkeTeF <erkeTeFo@users.noreply.github.com>
2019-05-05 05:02:44 +0200

    Revert "Substract cost"
    This reverts commit fb9d935177cee04899dd44558bf31c71abf0ac06.

--- MedWarGame/Assets/Code/Scripts/SpawnController.cs
+++ MedWarGame/Assets/Code/Scripts/SpawnController.cs
@@ -7,12 +7,10 @@ public class SpawnController : MonoBehaviour
     [SerializeField] private GameObject treesParent =  default;
     private treeScript[] trees;
     [SerializeField] private TroopSelectionManager troopSelection = default;
-    private CurrencyManager currencyManager;
     // Start is called before the first frame update
     void Start()
     {
         trees = treesParent.GetComponentsInChildren<treeScript>();
-        currencyManager = GameObject.Find("GameManager").GetComponent<CurrencyManager>();
     }
 
     private treeScript GetActiveTree()
@@ -30,12 +28,9 @@ public class SpawnController : MonoBehaviour
         Vector3 treePos = GetActiveTree().gameObject.transform.position;
         float deployRange = troopSelection.GetSelectedTroop().GetDeployRange();
         GameObject troopPrefab = troopSelection.GetSelectedTroopPrefab();
-        if (!PauseAndOptionsManager.gameIsPaused && currencyManager.currentCurrency >= troopSelection.GetSelectedTroopPrefab().GetComponent<troopScript>().GetCost())
-        {
+
+        if(!PauseAndOptionsManager.gameIsPaused)
             Instantiate(troopPrefab, GenerateRandomPosition(treePos, deployRange), Quaternion.identity);
-            currencyManager.currentCurrency -= troopSelection.GetSelectedTroopPrefab().GetComponent<troopScript>().GetCost();
-        }
-            
     }
 
     private Vector3 GenerateRandomPosition(Vector3 treePos, float deployRange)
